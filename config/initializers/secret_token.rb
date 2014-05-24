a85e104b95fe2624d20484d1a17b21976d7a915a
kan.akutsu <sakamotoshizuya@gmail.com>
2014-05-24 15:32:43 +0900

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SamuralApp::Application.config.secret_key_base = '3777df3fd7dfc4609ec82b13bdcf78878b9dfd010a39bf8c8b75914622e8fac6199a1077392c9d1622ea6d5f1b5c083336c05646126e50e3da348e53fc94f2a7'
